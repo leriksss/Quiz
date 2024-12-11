69772f8dde96d6cfe7e580396fccccd15bdb41ab
Ns05akhnez <beregovenkosofia@gmail.com>
2024-12-11 18:25:10 +0200

    створили 2 та 3 сторінку

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+let button = document.querySelector('.button');
+let start = document.querySelector('.start');
+let btn1 = document.querySelector('.btn-1');
+let btn2 = document.querySelector('.btn-2');
+let btn3 = document.querySelector('.btn-3');
+let question-easy = document.querySelector('.quetion-easy');
+let question-middle = document.querySelector('.quetion-middle');
+let question-hard = document.querySelector('.quetion-hard');
 
+
+let easy = {
+    0: ["Where are you from?", "Ukraine","IDK","Romania"],
+    1: ["Where do you sleep at night?", "Bed", "Table", "Chair"],
+    2: ["How many legs does a cat have?", "Four","Three","Five"],
+    3: ["What do you use to write?", "Pen", "A book", "A cup"],
+    4: ["What colors are bananas??", "Yellow", "Blue", "Purple"]
+}
+
+let middle = {
+    0: ["What day comes after Monday?", "Tuesday","Wednesday","Saturday"],
+    1: ["Which country is famous for pizza?", "Italy","Japan","Ukraine"],
+    2: ["What is the synonym of HAPPY?", "Glad","Angry","Sad"],
+    3: ["What is the capital Of France?", "Paris","Tokyo","Rome"],
+    4: ["What do you need to open the door?", "Key","Pen","Phone"]
+}
+let hard = {
+    0: ["What does 'How are you?' mean?", "How do feel?","What is your name?","Where are you from?"],
+    1: ["Which sentence is correct?", "She goes to school","She going to school","She go to school"],
+    2: ["What does 'break the ice' mean?", "To start a conversation","To destroy something frozen","To make someone angry"],
+    3: ["What is a capital of Australia?", "Canberra","Sydney","Melbourne"],
+    4: ["What does 'piece of cake' mean?", "Something very easy","Something delicious","A part of dessert"]
+}
+
+    
+    
